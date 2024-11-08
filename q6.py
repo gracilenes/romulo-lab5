892b553973c0232f514765772b32a0fa7de80ff7
gracilenes <gracilenes876@gmail.com>
2024-11-08 17:33:53 +0000

    atividade

--- q6.py
+++ q6.py
@@ -1,2 +1,16 @@
 """Verificação de Triângulo: Peça ao usuário o comprimento de três lados e verifique se eles podem formar um triângulo. Se sim, determine se é um triângulo equilátero, isósceles ou escaleno."""
 
+print("Verificação dos lados do triângulo")
+ladoA = int(input("Digite o lado A:"))
+ladoB = int(input("Digite o lado A:"))
+ladoC = int(input("Digite o lado A:"))
+
+if ladoA >= ladoB + ladoC or ladoB >= ladoA + ladoC or ladoC >= ladoA + ladoB:
+    print("Não é um triâmgulo válido") 
+    
+if ladoA == ladoB and ladoA == ladoC:
+    print("EQUILÁTERO")
+elif ladoA == ladoB or ladoA == ladoC or ladoB == ladoC:
+    print("iSÓSCELES")
+else:
+    print("ESCALENO")
---
+++ q8.py
@@ -0,0 +1,3 @@
+"""Validação de Login: Crie um programa que pede ao usuário um nome de usuário e uma senha. Se o nome de usuário for "admin" e a senha for "12345", exiba "Acesso concedido", caso contrário, exiba "Acesso negado"."""
+
+usuario = input("Digite o nome do usuário:")
